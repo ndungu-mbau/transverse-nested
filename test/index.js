3c003dbbe93a427de470790a81e8b95af883483e
Nelson Mbau <mbau.ndungu@gmail.com>
2020-02-19 17:29:56 +0300

    feat: Add tests

--- test/index.js
+++ test/index.js
@@ -1,14 +1,24 @@
-import { assert } from 'chai';
-import defaultAwesomeFunction, { awesomeFunction } from '../src';
+import { assert } from 'chai'
+import transverse from '../src'
 
-describe('Awesome test.', () => {
-  it('should test default awesome function', () => {
-    const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh';
-    assert(defaultAwesomeFunction('Dinesh') === expectedVal, 'Default not awesome :(');
-  });
+describe('Transverse Nested Test.', () => {
+  it('should get a nested value from the object', () => {
+    const obj = {
+      hi:{
+        hello:{
+          world:{
+            my:{
+              name:{
+                is:"Nelson"
+              }
+            }
+          }
+        }
+      }
+    }
 
-  it('should test awesome function', () => {
-    const expectedVal = 'I am just an Awesome Function';
-    assert(awesomeFunction() === expectedVal, 'Named awesome :(');
-  });
-});
+    const expectedVal = 'Nelson'
+
+    assert(transverse(obj, "hi.hello.world.my.name.is") === expectedVal, "Doesn't fetch nested prop")
+  })
+})
